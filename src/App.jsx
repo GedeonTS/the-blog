fc7c768ce4dc91e63ba32fd3a323893cd8399998
GedeonTS <gedeontshobohwa1@gmail.com>
2024-07-08 20:19:17 +0200

    Add first blog routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import Homepage from './pages/Homepage';
+import Blog from './pages/Blog';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-    <Homepage/>
-    </>
+    <Routes>
+    <Route exact path='/' element={ <Homepage/>} />
+    <Route exact path='/' element={ <Blog/>} />
+    </Routes>
   )
 }
 
