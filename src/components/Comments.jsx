f811414da0109e258723cb9f2808d8e8a91a5347
GedeonTS <gedeontshobohwa1@gmail.com>
2024-07-18 11:15:30 +0200

    Update comments logic

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-function Comments() {
+function Comment({value}) {
+  const mockValue={
+    message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius recusandae repudiandae, vero excepturi dolorem cumque sunt suscipit impedit modi ad blanditiis, ullam odio eveniet ut repellat maxime. Qui, ullam a?",
+    replies:["Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius recusandae repudiandae, vero excepturi dolorem cumque sunt suscipit impedit modi ad blanditiis, ullam odio eveniet ut repellat maxime. Qui, ullam a?"]
+  }
   return (
-    <div>
-      
+    <>
+    <div> 
+      {mockValue.message}
+    </div>
+    {
+      mockValue.replies.length && <Comments comments={mockValue.replies} open={true} />
+    }
+    </>
+  )
+}
+
+function Comments({comments, open}) {
+  return (
+    <div className='comments-wrapper'>
+      {
+        comments.map(
+          Comment => <Comment key />
+        )
+      }
     </div>
   )
 }
