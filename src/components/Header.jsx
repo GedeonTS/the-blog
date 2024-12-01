6ee0ade4289558b720f82c76780937288802a9bb
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-12-01 19:07:23 +0200

    Create primary color

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full p-4 flex justify-between">
+      <h1>Ushindi's Blog</h1>
+    </div>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#f8f5fa",
+          100: "#efeaf4",
+          200: "#dad1e6",
+          300: "#b9a9d0",
+          400: "#927ab6",
+          500: "#76599e",
+          600: "#614683",
+          700: "#50396b",
+          800: "#44325a",
+          900: "#3c2e4c",
+          950: "#100c14",
+        },
+      },
+    },
   },
   plugins: [],
 };
